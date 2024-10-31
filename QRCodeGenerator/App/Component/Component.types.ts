648abde9d5e7654f57d32d67d5cb4e073cface4f
Kalel Poeta <kalel.poeta@orbisag.ch>
2024-10-31 10:30:41 +0100

    Export types

--- QRCodeGenerator/App/Component/Component.types.ts
+++ QRCodeGenerator/App/Component/Component.types.ts
@@ -1,5 +1,11 @@
-export interface IQRCodeGenrops {
+export interface IQRCodeGenProps {
   // These are set based on the toggles shown above the examples (not needed in real code)
   buttonValue: string;
   buttonLink: string;
+  infoLabels: {
+    name: string | null;
+    category: ComponentFramework.LookupValue[] | null;
+    parentAsset: ComponentFramework.LookupValue[] | null;
+    product: ComponentFramework.LookupValue[] | null;
+  };
 }
